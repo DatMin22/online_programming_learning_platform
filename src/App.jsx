4f8303e456fbf5ae095f3e1a758d72e1e6536f28
NgoTruongAn <anngo091101@gmail.com>
2024-01-17 22:48:32 +0700

    test function delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { HomePage } from './pages/HomePage/HomePage'
-import UserLayout from './layouts/UserLayout'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/HomePage/HomePage";
+import UserLayout from "./layouts/UserLayout";
+import AdminLayout from "./layouts/AdminLayout/AdminLayout";
+import { AdminPage } from "./pages/AdminPage/AdminPage";
+
 
 function App() {
-
   return (
     <BrowserRouter>
       {/* <Header /> */}
       <Routes>
-
-
         {/* Nest Router */}
-        <Route path='/' element={<UserLayout/>}>
+        <Route path="/" element={<UserLayout />}>
           <Route index element={<HomePage />} />
 
           {/* <Route path={PATH.CHANGE_PASSWORD} element={<ChangePass />} /> */}
-
-
-
-
           {/* Dynamic segments */}
+
           {/* <Route path='animal/:id' element={<AnimalDetails />} /> */}
         </Route>
-
-
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminPage />} />
+        </Route>
       </Routes>
 
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
